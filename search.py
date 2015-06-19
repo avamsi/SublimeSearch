57c8d217645988e728703e9237ed48aa94e1c5b4
krikx <vamsi_ism@outlook.com>
2015-06-19 14:56:09 +0530

    Minor Changes.

--- search.py
+++ search.py
@@ -1,9 +1,6 @@
 import sublime, sublime_plugin
 import webbrowser
 
-settings = sublime.load_settings('Search.sublime-settings')
-search_engine = settings.get('search_engine', 'Google')
-
 url = {
     'Bing': 'https://www.bing.com/search?q=',
     'DuckDuckGo': 'https://duckduckgo.com/?q=',
@@ -12,10 +9,14 @@ url = {
 }
 
 class WebSearchCommand(sublime_plugin.TextCommand):
+    def __init__(self):
+        settings = sublime.load_settings('Search.sublime-settings')
+        self.search_engine = settings.get('search_engine', 'Google')
+
     def run(self, edit):
         text = self.get_text()
         webbrowser.open_new_tab(
-            url[search_engine] + text
+            url[self.search_engine] + text
         )
 
     def is_visible(self):
@@ -27,7 +28,7 @@ class WebSearchCommand(sublime_plugin.TextCommand):
         return self.view.substr(reg).strip()
 
     def description(self):
-        text = "%s for '%s'" % (search_engine, self.get_text())
+        text = "%s for '%s'" % (self.search_engine, self.get_text())
         if len(text) > 32:
             text = text[0: 32] + '...'
         return 'Search ' + text
